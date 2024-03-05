c3c89607c304d91151148ecd08d38cfd74d8a15e
Johnatan de Souza <johnatan.desouza@charlestonplace.com>
2024-03-05 00:27:43 -0500

    tipCalculator

---
+++ tipCalculator.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip calculator.")
+initialBill = float(input("What's the total bill? "))
+numberOfPeople = float(input("How many people are splitting the bill? "))
+tipPercentage = float(input("What percentage tip would you like to give? "))
+
+# Calculate the impact the percentage given will have on the bill
+maximumTip = 100
+tipAmmount = (tipPercentage / maximumTip) * 100
+
+#Calculate the final ammount considering all previous values provided 
+finalBill = (tipAmmount + initialBill) / numberOfPeople
+
+print(f"Each person should pay {finalBill}")
