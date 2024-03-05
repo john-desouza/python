d54b1d77aa23a6756d425d279745e2eeaf29fb7c
Johnatan de Souza <johnatan.desouza@charlestonplace.com>
2024-03-04 22:07:16 -0500

    bandNameGenerator

---
+++ bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator! Type your answers and press enter below:")
+city = input("What's the name of the city where you grew up in?\nAnswer: ")
+pet = input("What's your pet's name?\nAnswer: ")
+print(f"The name of your band is: {city} {pet}")
